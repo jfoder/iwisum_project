ae8e8cadf82e1bb7051aa6858d0746df7859da92
misikora <mikolajsi@student.agh.edu.pl>
2022-01-06 19:01:15 +0100

    Add KBandit algorithm

--- src/main/java/pl/agh/edu/BattleRunner.java
+++ src/main/java/pl/agh/edu/BattleRunner.java
@@ -1,5 +1,7 @@
 package pl.agh.edu;
 
+import pl.agh.edu.robots.OffensiveRobot;
+import robocode.AdvancedRobot;
 import robocode.control.BattleSpecification;
 import robocode.control.BattlefieldSpecification;
 import robocode.control.RobocodeEngine;
@@ -9,22 +11,32 @@ import robocode.control.events.BattleCompletedEvent;
 import robocode.control.events.BattleErrorEvent;
 import robocode.control.events.BattleMessageEvent;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BattleRunner {
 
     public static void main(String[] args) {
 
+        List<AdvancedRobot> bots = new ArrayList<>();
+        bots.add(new OffensiveRobot());
+        KBandit kBandit = new KBandit(bots);
         System.setProperty("TESTING", "true");
         RobocodeEngine.setLogMessagesEnabled(false);
         RobocodeEngine engine = new RobocodeEngine(new java.io.File("C:/robocode"));
-        engine.addBattleListener(new BattleObserver());
+        for (int i = 0; i < 100; i++) {
+            AdvancedRobot currentBot = kBandit.chooseBot();
+            System.out.println("Chosen bot: " + currentBot.getClass().getCanonicalName());
+            engine.addBattleListener(new BattleObserver(kBandit));
 
-        int numberOfRounds = 5;
-        BattlefieldSpecification battlefield = new BattlefieldSpecification(800, 600);
-        RobotSpecification[] selectedRobots = engine.getLocalRepository("pl.agh.edu.OffensiveRobot,sample.RamFire");
+            int numberOfRounds = 5;
+            BattlefieldSpecification battlefield = new BattlefieldSpecification(800, 600);
+            RobotSpecification[] selectedRobots = engine.getLocalRepository("sample.RamFire," + currentBot.getClass().getCanonicalName());
 
-        BattleSpecification battleSpec = new BattleSpecification(numberOfRounds, battlefield, selectedRobots);
+            BattleSpecification battleSpec = new BattleSpecification(numberOfRounds, battlefield, selectedRobots);
 
-        engine.runBattle(battleSpec, true);
+            engine.runBattle(battleSpec, true);
+        }
         engine.close();
         System.exit(0);
     }
@@ -32,12 +44,21 @@ public class BattleRunner {
 
 class BattleObserver extends BattleAdaptor {
 
+    private KBandit kBandit;
+
+    public BattleObserver(KBandit kBandit) {
+        this.kBandit = kBandit;
+    }
+
     public void onBattleCompleted(BattleCompletedEvent e) {
         System.out.println("-- Battle has completed --");
 
         System.out.println("Battle results:");
         for (robocode.BattleResults result : e.getSortedResults()) {
             System.out.println("  " + result.getTeamLeaderName() + ": " + result.getScore());
+            if (result.getTeamLeaderName().equals(this.kBandit.getCurrentBot().getClass().getCanonicalName())) {
+                kBandit.learn(result.getScore());
+            }
         }
     }
 
